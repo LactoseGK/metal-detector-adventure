36b2dee61ab5263fb28ccc2188e690a08631b6e1
Geir-Kåre S. Wærp <geir-kare.waerp@shortcut.no>
2018-06-08 13:14:57 +0200

    Initialized project in Xcode.

---
+++ MetalDetectorAdventure/MetalDetectorAdventure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalDetectorAdventure
+//
+//  Created by Geir-Kåre S. Wærp on 08/06/2018.
+//  Copyright © 2018 Geir-Kåre S. Wærp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
